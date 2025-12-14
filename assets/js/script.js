a3cd59a8a5386d532c817e7f4c86c6ff8c2f72f5
leilacsak <leilacsak@gmail.com>
2025-12-14 15:40:25 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+function (runGame) {
+  
+}
+
+function (chekcAnswer) {
+}
+
+function (calculateCorrectAnswer) {
+}
+
+function (incrementScore) {
+
+}
+
+function (incrementWrongAnswer) {}
+
+function (displayAdditionQuestion) {}
+
+function (displaySubtractQuestion) {}
+
+function (displayMultiplyQuestion) {}
